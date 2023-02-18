be95d08a2008e4e211c90ae83b0a1e45a7ccbed1
Wesley Willian <wesleywilliancg@gmail.com>
2023-02-18 02:11:24 -0300

    asd

--- js/Produtos.js
+++ js/Produtos.js
@@ -16,7 +16,7 @@ $(document).ready(function(){
      });
 
      var typed = new Typed(".typing", {
-         strings:["Aqui você encontra os melhores produtos!"],
+         strings:["aqui você encontra os melhores produtos!"],
          typeSpeed:100,
          backSpeed:100,
          loop:true
